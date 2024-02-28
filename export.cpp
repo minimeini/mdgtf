23577ebd235eecbd7b404a08f2a32a9c7799a7b1
Meini Tang <meini.tang@gmail.com>
2024-02-27 16:53:12 -0800

    model

--- export.cpp
+++ export.cpp
@@ -8,152 +8,6 @@ using namespace Rcpp;
 // [[Rcpp::plugins(cpp17)]]
 // [[Rcpp::depends(RcppArmadillo,nloptr, BH)]]
 
-void parse_model_settings(
-    std::string &obs_dist,
-    std::string &link_func,
-    std::string &trans_func,
-    std::string &gain_func,
-    std::string &lag_dist,
-    std::string &err_dist,
-    const Rcpp::List &Model)
-{
-    Rcpp::List model = Model;
-    if (model.containsElementNamed("obs_dist"))
-    {
-        obs_dist = tolower(Rcpp::as<std::string>(model["obs_dist"]));
-    }
-    else
-    {
-        obs_dist = "nbinom";
-    }
-
-    if (model.containsElementNamed("link_func"))
-    {
-        link_func = tolower(Rcpp::as<std::string>(model["link_func"]));
-    }
-    else
-    {
-        link_func = "identity";
-    }
-
-    if (model.containsElementNamed("trans_func"))
-    {
-        trans_func = tolower(Rcpp::as<std::string>(model["trans_func"]));
-    }
-    else
-    {
-        trans_func = "sliding";
-    }
-
-    if (model.containsElementNamed("gain_func"))
-    {
-        gain_func = tolower(Rcpp::as<std::string>(model["gain_func"]));
-    }
-    else
-    {
-        gain_func = "softplus";
-    }
-
-    if (model.containsElementNamed("lag_dist"))
-    {
-        lag_dist = tolower(Rcpp::as<std::string>(model["lag_dist"]));
-    }
-    else
-    {
-        lag_dist = "lognorm";
-    }
-
-    if (model.containsElementNamed("err_dist"))
-    {
-        err_dist = tolower(Rcpp::as<std::string>(model["err_dist"]));
-    }
-    else
-    {
-        err_dist = "gaussian";
-    }
-
-    return;
-}
-
-
-void parse_dimension_settings(
-    unsigned int &nlag,
-    unsigned int &ntime,
-    bool &truncated,
-    const Rcpp::List &Dimension)
-{
-    Rcpp::List dim = Dimension;
-
-    if (dim.containsElementNamed("nlag"))
-    {
-        nlag = Rcpp::as<unsigned int>(dim["nlag"]);
-    }
-    else
-    {
-        nlag = 10;
-    }
-
-    if (dim.containsElementNamed("ntime"))
-    {
-        ntime = Rcpp::as<unsigned int>(dim["ntime"]);
-    }
-    else
-    {
-        ntime = 200;
-    }
-
-    if (nlag >= ntime)
-    {
-        nlag = ntime;
-        truncated = false;
-    }
-    else if (dim.containsElementNamed("truncated"))
-    {
-        truncated = Rcpp::as<bool>(dim["truncated"]);
-    }
-    else
-    {
-        truncated = true;
-    }
-}
-
-void parse_param_settings(
-    Rcpp::NumericVector &obs,
-    Rcpp::NumericVector &lag,
-    Rcpp::NumericVector &err,
-    const Rcpp::List &Param)
-{
-    Rcpp::List param = Param;
-
-    if (param.containsElementNamed("obs"))
-    {
-        obs = Rcpp::as<Rcpp::NumericVector>(param["obs"]);
-    }
-    else
-    {
-        obs = Rcpp::NumericVector::create(0., 30.);
-    }
-
-    if (param.containsElementNamed("lag"))
-    {
-        lag = Rcpp::as<Rcpp::NumericVector>(param["lag"]);
-    }
-    else
-    {
-        lag = Rcpp::NumericVector::create(1.4, 0.3);
-    }
-
-    if (param.containsElementNamed("err"))
-    {
-        err = Rcpp::as<Rcpp::NumericVector>(param["err"]);
-    }
-    else
-    {
-        err = Rcpp::NumericVector::create(0.01, 0.);
-    }
-
-    return;
-}
 
 //' @export
 // [[Rcpp::export]]
@@ -290,38 +144,15 @@ Rcpp::List dgtf_model(
 }
 
 
-Model dgtf_initialize(const Rcpp::List &settings)
-{
-    Rcpp::List model_settings = settings["model"];
-    std::string obs_dist, link_func, trans_func, gain_func, lag_dist, err_dist;
-    parse_model_settings(
-        obs_dist, link_func, trans_func,
-        gain_func, lag_dist, err_dist,
-        model_settings);
 
-    Rcpp::List dim_settings = settings["dim"];
-    unsigned int nlag, ntime;
-    bool truncated;
-    parse_dimension_settings(nlag, ntime, truncated, dim_settings);
-
-    Rcpp::List param_settings = settings["param"];
-    Rcpp::NumericVector lag_param, obs_param, err_param;
-    parse_param_settings(obs_param, lag_param, err_param, param_settings);
-
-    Dim dim(ntime, nlag, lag_param[1]);
-    Model model(
-        dim, obs_dist, link_func, gain_func, lag_dist, err_dist,
-        obs_param, lag_param, err_param, trans_func);
-
-    return model;
-}
 
 
 //' @export
 // [[Rcpp::export]]
 Rcpp::List dgtf_simulate(const Rcpp::List &settings)
 {
-    Model model = dgtf_initialize(settings);
+    Model model(settings);
+    // Model model = dgtf_initialize(settings);
     arma::vec ysim = model.simulate();
     arma::vec psi = model.transfer.fgain.psi;
     arma::vec wt = model.derr.wt;
@@ -343,7 +174,8 @@ Rcpp::List dgtf_infer(
     const Rcpp::List &method_settings,
     const bool &summarize = true)
 {
-    Model model = dgtf_initialize(model_settings);
+    // Model model = dgtf_initialize(model_settings);
+    Model model(model_settings);
     std::map<std::string, AVAIL::Algo> algo_list = AVAIL::algo_list;
     std::string algo_name = tolower(method);
 
@@ -416,4 +248,3 @@ Rcpp::List dgtf_infer(
 
     return output;
 }
-
