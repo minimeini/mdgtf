c6c4f1db1cbdebd834b814ce78ee44c49992b9bf
Meini Tang <meini.tang@gmail.com>
2024-08-28 13:48:16 -0700

    clean up redundant functions

--- ErrDist.hpp
+++ ErrDist.hpp
@@ -24,7 +24,7 @@
 class ErrDist : public Dist
 {
 public:
-    ErrDist() : wt(_wt), psi(_psi) 
+    ErrDist()
     {
         init_default();
         return;
@@ -35,7 +35,7 @@ public:
         const std::string &err_dist,
         const double &par1_in,
         const double &par2_in
-    ) : wt(_wt), psi(_psi)
+    )
     {
         init(err_dist, par1_in, par2_in);
     }
@@ -60,10 +60,6 @@ public:
         par2 = 0.0; // w[0]
         return;
     }
-
-
-    const arma::vec &wt;
-    const arma::vec &psi;
 
 
     /**
@@ -132,78 +128,9 @@ public:
         return output;
     }
 
-    /**
-     * @brief Draw samples from the random-walk process, return either white noise w[t] or the cumulative error psi[t].
-     *
-     * @param nT Number of samples we want to draw.
-     * @param W Variance of the random-walk process.
-     * @param w0 Initial value of the random-walk process.
-     * @param cumsum Reterun psi if true; otherwise return wt.
-     * @return arma::vec
-     */
-    void sample(
-        const unsigned int &nT,
-        const bool &cumsum = true,
-        const Rcpp::Nullable<Rcpp::NumericVector> &wt_init = R_NilValue
-    )
-    {
-        std::map<std::string, AVAIL::Dist> err_list = ErrDist::err_list;
-        _wt.set_size(nT + 1);
-        _wt.zeros();
-
-        if (par1 > 0)
-        {
-            switch (err_list[name])
-            {
-            case AVAIL::Dist::gaussian:
-            {
-
-                double Wsd = std::sqrt(par1);
-                _wt.randn();
-                _wt.for_each([&Wsd](arma::vec::elem_type &val)
-                             { val *= Wsd; });
-                _wt.at(0) = par2;
-                break;
-            }
-            case AVAIL::Dist::constant:
-            {
-                _wt.zeros();
-                if (wt_init.isNull())
-                {
-                    throw std::invalid_argument("Constant states undefined.");
-                }
-                else
-                {
-                    _wt_init = Rcpp::as<arma::vec>(wt_init);
-                    unsigned int nelem = std::min(_wt_init.n_elem, _wt.n_elem);
-                    _wt.head(nelem) = _wt_init;
-                }
-                break;
-            }
-            default:
-            {
-                throw std::invalid_argument("Undefined.");
-                break;
-            }
-            }
-        }
-
-        
-
-        if (cumsum)
-        {
-            _psi = arma::cumsum(_wt);
-        }
-        return;
-    }
 
     static const std::map<std::string, AVAIL::Dist> err_list;
 private:
-    unsigned int _nT;
-    arma::vec _wt;
-    arma::vec _psi;           // Initial value (at time = 0) of the normal errors.
-    arma::vec _wt_init;
-
     static std::map<std::string, AVAIL::Dist> map_err_dist()
     {
         std::map<std::string, AVAIL::Dist> ERR_MAP;
