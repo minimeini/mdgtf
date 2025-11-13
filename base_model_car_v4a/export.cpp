e760d0c05174c8566b55f7e1ed9645ebc854398c
Meini Tang <meini.tang@gmail.com>
2025-11-13 10:21:10 -0800

    v4a

--- base_model_car_v4a/export.cpp
+++ base_model_car_v4a/export.cpp
@@ -38,7 +38,7 @@ Rcpp::List mdgtf_default_algo_settings(const std::string &method)
     }
 
     return opts;
-}
+} // end of mdgtf_default_algo_settings()
 
 
 //' @export
@@ -60,7 +60,7 @@ Rcpp::List mdgtf_simulate(
         Rcpp::Named("Rt") = GainFunc::psi2hpsi<arma::mat>(Psi, model.fgain),
         Rcpp::Named("model") = settings
     );
-}
+} // end of mdgtf_simulate()
 
 
 //' @export
@@ -97,4 +97,101 @@ Rcpp::List mdgtf_infer(
     std::cout << "\nElapsed time: " << duration.count() << " microseconds" << std::endl;
 
     return output;
-}
+} // end of mdgtf_infer()
+
+
+//' @export
+// [[Rcpp::export]]
+Rcpp::List mdgtf_posterior_predictive(
+    const Rcpp::List &output, 
+    const Rcpp::List &model_opts, 
+    const arma::mat &Y, // nS x (nT + 1)
+    const unsigned int &nrep = 1,
+    const Rcpp::Nullable<Rcpp::NumericMatrix> &Rt = R_NilValue // nS x (nT + 1)
+)
+{
+    Model model(model_opts);
+    arma::cube Y_pred, Y_residual, hPsi;
+    // Y_pred: nS x (nT + 1) x (nsample * nrep)
+    // Y_residual: nS x (nT + 1) x nsample
+    // hPsi: nS x (nT + 1) x nsample
+    double chi_sqr = model.sample_posterior_predictive_y(Y_pred, Y_residual, hPsi, output, Y, nrep);
+
+    arma::cube Y_pred_trunc = Y_pred.cols(1, Y_pred.n_cols - 1); // nS x nT x (nsample * nrep)
+    arma::mat Y_trunc = Y.cols(1, Y.n_cols - 1); // nS x nT
+    arma::vec crps = calculate_crps(Y_trunc, Y_pred_trunc);
+
+    arma::vec prob = {0.025, 0.5, 0.975};
+    arma::cube Y_pred_ci(Y_pred.n_rows, Y_pred.n_cols, prob.n_elem);
+    arma::vec Y_coverage(Y_pred.n_rows, arma::fill::zeros);
+    arma::vec Y_width(Y_pred.n_rows, arma::fill::zeros);
+    for (unsigned int i = 0; i < Y_pred.n_rows; i++)
+    {
+        for (unsigned int j = 0; j < Y_pred.n_cols; j++)
+        {
+            arma::vec samples = Y_pred.tube(i, j); // (nsample * nrep) x 1
+            Y_pred_ci.tube(i, j) = arma::quantile(samples, prob); // 3 x 1
+        }
+
+        arma::vec lobnd = Y_pred_ci.slice(0).row(i).t();
+        arma::vec upbnd = Y_pred_ci.slice(2).row(i).t();
+        arma::vec true_val = Y.row(i).t();
+
+        double covered = arma::accu((true_val >= lobnd) % (true_val <= upbnd));
+        Y_coverage.at(i) = covered / static_cast<double>(true_val.n_elem);
+        Y_width.at(i) = arma::mean(upbnd - lobnd);
+    }
+
+    Rcpp::List stats = Rcpp::List::create(
+        Rcpp::Named("Y_pred") = Y_pred_ci, // nS x (nT + 1) x 3
+        Rcpp::Named("Y_residual") = Y_residual,
+        Rcpp::Named("chi_sqr_avg") = chi_sqr,
+        Rcpp::Named("crps") = crps, // nS x 1
+        Rcpp::Named("crps_avg") = arma::mean(crps),
+        Rcpp::Named("Y_coverage") = Y_coverage, // nS x 1
+        Rcpp::Named("Y_coverage_avg") = arma::mean(Y_coverage),
+        Rcpp::Named("Y_width") = Y_width, // nS x 1
+        Rcpp::Named("Y_width_avg") = arma::mean(Y_width)
+    );
+
+    if (Rt.isNotNull())
+    {
+        arma::mat Rt_true = Rcpp::as<arma::mat>(Rt);
+
+        arma::cube hPsi_ci(hPsi.n_rows, hPsi.n_cols, prob.n_elem);
+        arma::vec hPsi_coverage(hPsi.n_rows, arma::fill::zeros);
+        arma::vec hPsi_width(hPsi.n_rows, arma::fill::zeros);
+        for (unsigned int i = 0; i < hPsi.n_rows; i++)
+        {
+            for (unsigned int j = 0; j < hPsi.n_cols; j++)
+            {
+                arma::vec samples = hPsi.tube(i, j);                // nsample x 1
+                hPsi_ci.tube(i, j) = arma::quantile(samples, prob); // 3 x 1
+            }
+
+            if (Rt.isNotNull())
+            {
+                arma::vec lobnd = hPsi_ci.slice(0).row(i).t();
+                arma::vec upbnd = hPsi_ci.slice(2).row(i).t();
+                arma::vec true_val = Rt_true.row(i).t();
+
+                double covered = arma::accu((true_val >= lobnd) % (true_val <= upbnd));
+                hPsi_coverage.at(i) = covered / static_cast<double>(true_val.n_elem);
+                hPsi_width.at(i) = arma::mean(upbnd - lobnd);
+            }
+        }
+        arma::cube Rt_res_all = arma::abs(hPsi.each_slice() - Rt_true);
+        arma::mat Rt_res = arma::mean(Rt_res_all, 2); // nS x (nT + 1)
+        arma::vec mae_Rt = arma::mean(Rt_res, 1);
+
+        stats["Rt"] = hPsi_ci; // nS x (nT + 1) x 3
+        stats["Rt_mae"] = mae_Rt; // nS x 1
+        stats["Rt_mae_avg"] = arma::mean(mae_Rt);
+        stats["Rt_coverage"] = hPsi_coverage; // nS x 1
+        stats["Rt_coverage_avg"] = arma::mean(hPsi_coverage);
+        stats["Rt_width"] = hPsi_width; // nS x 1
+        stats["Rt_width_avg"] = arma::mean(hPsi_width);
+    }
+
+    return stats;
+} // end of mdgtf_posterior_predictive()
